a3c839be5da72adda2a204237ed41de0b95f8bb5
pzeg01231 <19achraf@gmail.com>
2024-05-08 10:56:35 +0100

    Added Track Entity in main branch

---
+++ src/main/java/railway/moroccorailwaysystem/model/Track.java
@@ -0,0 +1,9 @@
+package railway.moroccorailwaysystem.model;
+
+import java.time.LocalDate;
+
+public class Track {
+    String trackNumber;
+    LocalDate established;
+    boolean isTrackActive;
+}
