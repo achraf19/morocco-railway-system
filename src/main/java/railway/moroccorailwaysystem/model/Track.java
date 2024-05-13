964e0ecec856fd7b853281be828aa8899dda5963
pzeg01231 <19achraf@gmail.com>
2024-05-13 16:15:58 +0100

    Deleted Track entity

--- src/main/java/railway/moroccorailwaysystem/model/Track.java
+++
@@ -1,9 +0,0 @@
-package railway.moroccorailwaysystem.model;
-
-import java.time.LocalDate;
-
-public class Track {
-    String trackNumber;
-    LocalDate established;
-    boolean isTrackActive;
-}
